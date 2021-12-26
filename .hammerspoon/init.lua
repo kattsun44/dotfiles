6c9cd2a9dab949cd45b421fe776cc73dad97a88f
kattsun44 <kattsun4416@gmail.com>
2021-12-26 14:57:35 +0900

    hammerspoon設定ファイル共有

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,8 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+    left = {{ 'ctrl', 'shift', 'cmd' }, 'left' },
+    right = {{ 'ctrl', 'shift', 'cmd' }, 'right' },
+    -- up = {{ 'ctrl', 'shift', 'cmd' }, 'up' },
+    down = {{ 'ctrl', 'shift', 'cmd' }, 'down' },
+    maximum = {{ 'ctrl', 'shift', 'cmd' }, 'up' },
+});
